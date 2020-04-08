7d25f68592427a3468e38167a97869674f91fb35
silvioBi <biasiol.silvio@gmail.com>
2020-04-08 18:25:17 +0200

    Updates

---
+++ precache-manifest.6f7ec9ca0c1fa8632d59db2412d8db27.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d45ca28667dcd38de0ac25b8f32dc89",
+    "url": "/mmcriscuolo/index.html"
+  },
+  {
+    "revision": "68131ee1a78d4d7d2c03",
+    "url": "/mmcriscuolo/static/css/main.0c96a83e.chunk.css"
+  },
+  {
+    "revision": "71c8000fae062fcd2ff2",
+    "url": "/mmcriscuolo/static/js/2.41ccd40e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mmcriscuolo/static/js/2.41ccd40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68131ee1a78d4d7d2c03",
+    "url": "/mmcriscuolo/static/js/main.c015f639.chunk.js"
+  },
+  {
+    "revision": "2e074d99baab9954baf1",
+    "url": "/mmcriscuolo/static/js/runtime-main.4c9fd073.js"
+  },
+  {
+    "revision": "3395f695db1cc143f1b7c3683e922226",
+    "url": "/mmcriscuolo/static/media/artwork-1.3395f695.jpeg"
+  },
+  {
+    "revision": "4f43000c53dd9653a43d14ee3ec1872a",
+    "url": "/mmcriscuolo/static/media/facebook.4f43000c.svg"
+  },
+  {
+    "revision": "ba9f1ff348a706a936a8f0e2c0cbf9f8",
+    "url": "/mmcriscuolo/static/media/instagram.ba9f1ff3.svg"
+  },
+  {
+    "revision": "6fcd315d52dca6bf24667e0522c7ad32",
+    "url": "/mmcriscuolo/static/media/linkedin.6fcd315d.svg"
+  },
+  {
+    "revision": "cbcf7c4eb097431dbd797bdd50241f1f",
+    "url": "/mmcriscuolo/static/media/pinterest.cbcf7c4e.svg"
+  },
+  {
+    "revision": "c32a7e165e74f54ea46cff6c88136d5a",
+    "url": "/mmcriscuolo/static/media/twitter.c32a7e16.svg"
+  },
+  {
+    "revision": "c1deb6ce7be2b985b4487882c4a5462f",
+    "url": "/mmcriscuolo/static/media/whatsapp.c1deb6ce.svg"
+  }
+]);
